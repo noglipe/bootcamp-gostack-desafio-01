78a7ea3b1ed1622d63fe3e980e0d6e498e32939a
Filipe Nogueira <nog.lipe@gmail.com>
2019-08-21 23:00:43 -0300

    Finalizado Desafio 01!

--- index.js
+++ index.js
@@ -6,9 +6,37 @@ server.listen(3012);
 server.use(express.json());
 
 const projects = [];
+let contReq = 0;
+
+//Middlewares para verificar a existencia do projeto
+function checkIdInArray(req, res, next) {
+  const project = projects.find(project => project.id === req.params.id);
+  if (!project) {
+    return res.status(400).json({ error: "Projeto Não Existe!" });
+  }
+  return next();
+}
+
+//Middlewares para impedir dois projetos com mesmo ID
+function checkIdInArrayPost(req, res, next) {
+  const project = projects.find(project => project.id === req.body.id);
+  if (project) {
+    return res
+      .status(400)
+      .json({ error: `Já Existe o Projeto de ID: ${req.body.id}!` });
+  }
+  return next();
+}
+
+//Middlewares para realizar a contagem de requisições
+function contRequisicao(req, res, next) {
+  contReq++;
+  console.log(contReq);
+  return next();
+}
 
 //Adicionar Projetos
-server.post("/projects", (req, res) => {
+server.post("/projects", contRequisicao, checkIdInArrayPost, (req, res) => {
   const { id, title } = req.body;
   const addProjeto = {
     id,
@@ -21,29 +49,42 @@ server.post("/projects", (req, res) => {
 });
 
 //Adicionar tarefa ao projeto
-server.post("/projects/:id/:tasks/", (req, res) => {
-  //Percorrer todos os projetos cadastrados para alterar
-  //somente o de id específico
-  for (var project of projects) {
-    if (project.id === req.params.id) {
-      project.tasks.push(req.params.tasks);
+server.post(
+  "/projects/:id/:tasks/",
+  contRequisicao,
+  checkIdInArrayPost,
+  (req, res) => {
+    for (var project of projects) {
+      if (project.id === req.params.id) {
+        project.tasks.push(req.params.tasks);
+      }
     }
-  }
 
-  return res.json(projects);
-});
+    return res.json(projects);
+  }
+);
 
 //Exibir projetos
-server.get("/projects", (req, res) => {
+server.get("/projects", contRequisicao, (req, res) => {
   return res.json(projects);
 });
 
 //Atualizar Título de Projeto
-server.put("/projects/:id", (req, res) => {
-  for (var project of projects) {
+server.put("/projects/:id", contRequisicao, checkIdInArray, (req, res) => {
+  projects.map(project => {
     if (project.id === req.params.id) {
       project.title = req.body.title;
     }
-  }
+  });
+
+  return res.json(projects);
+});
+
+//Deletar um projeto
+server.delete("/projects/:id", contRequisicao, checkIdInArray, (req, res) => {
+  const delProjeto = projects.findIndex(
+    project => project.id === req.params.id
+  );
+  projects.splice(delProjeto, 1);
   return res.json(projects);
 });
